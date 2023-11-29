057277a4ee63f8ece5f0995c5675488e6b062524
Aadilol <128275184+Aadilol@users.noreply.github.com>
2023-11-28 20:32:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -201,8 +201,8 @@ function openImageWindow(windowId) {
     imageWindow.style.top = centerY + 'px';
 
     // Ensure the window stays within the bounds
-    var x = Math.max(0, Math.min(centerX, window.innerWidth - imageWindow.offsetWidth)) - 50;
-    var y = Math.max(0, Math.min(centerY, window.innerHeight - imageWindow.offsetHeight)) - 100;
+    var x = Math.max(0, Math.min(centerX, window.innerWidth - imageWindow.offsetWidth)) ;
+    var y = Math.max(0, Math.min(centerY, window.innerHeight - imageWindow.offsetHeight)) - 150;
 
     // Set the window's position
     imageWindow.style.left = x + 'px';
